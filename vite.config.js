38f7c4a46f67f2f9d3b03be6290ca4de05288152
SAQLAIN97i <mr.saqlain97i@gmail.com>
2026-01-28 17:06:46 +0530

    Refactor Vite config for consistency and clarity

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nexxus/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: true,
-  },
-})
+  base: "/Nexxus/",
+});
